982598c98bd9d5c89277e47efc77e7142c33353a
Alexgagrun <aleksandr.gargun@gmail.com>
2022-01-24 14:23:40 +0300

    fix the stucture of build

---
+++ precache-manifest.70b85f6c37f9cff352f5008bd12b03e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559839221ff0b4e4065735a7ebd41b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a2d8b3aefad8c4e3d90",
+    "url": "/static/css/main.9ee0148a.chunk.css"
+  },
+  {
+    "revision": "9ab1abd40986d5775cb3",
+    "url": "/static/js/2.e16cd107.chunk.js"
+  },
+  {
+    "revision": "9a2d8b3aefad8c4e3d90",
+    "url": "/static/js/main.b82c40da.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
